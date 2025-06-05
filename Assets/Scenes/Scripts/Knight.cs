04e920aae6dd52a75bdf0a3e41398889e1124b4a
Alexander Fountain <159776264+AlexCrombieF@users.noreply.github.com>
2025-06-05 13:28:21 +0000

    just editing some minor tweeks to knight
    Created acceleration paramotor so when the knight is hit it wont freeze as much during knockback just a minor tweak for gameplay ability, will be now moving on to making a range bow attack for the player

--- Assets/Scenes/Scripts/Knight.cs
+++ Assets/Scenes/Scripts/Knight.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody2D),typeof(TouchingDirections))]
 public class Knight : MonoBehaviour
 {
-    public float walkSpeed = 3f;
+    public float walkAcceleration = 3f;
+    public float maxSpeed = 3f;
     public float walkStopRate =  0.6f;
     public DetectionZone attackZone;
     public DetectionZone cliffDetectionZone;
@@ -86,7 +87,8 @@ public class Knight : MonoBehaviour
         if (!damagable.LockVelocity)
         {
             if (CanMove)
-                rb.velocity = new Vector2(walkSpeed * walkDirectionVector.x, rb.velocity.y);
+                //accerlation towards to max speed
+                rb.velocity = new Vector2(Mathf.Clamp(rb.velocity.x + (walkAcceleration * walkDirectionVector.x * Time.fixedDeltaTime), - maxSpeed, maxSpeed), rb.velocity.y);
             else
                 rb.velocity = new Vector2(Mathf.Lerp(rb.velocity.x, 0, walkStopRate), rb.velocity.y);
         }
